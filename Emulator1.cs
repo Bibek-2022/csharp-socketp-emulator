d6246e20f43cbb6f8e7839761dc375e7b99c424a
BIBEK SHRESTHA <71386018+Bibek-2022@users.noreply.github.com>
2023-01-20 15:37:08 +1100

    Add files via upload

---
+++ Emulator1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Linq;
+using System.IO;
+using System.Configuration;
+using System.Reflection;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Net;
+
+namespace ATSEmulator
+{
+    public class Emulator1
+    {
+
+     
+        public Emulator1()
+        {
+            try
+            {
+
+                string message = Console.ReadLine();
+                int port = 11000;
+
+                IPAddress ipAddress = IPAddress.Parse("10.22.6.33");
+
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+                Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                sender.Connect(remoteEP);
+
+                Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(message);
+
+                int bytesSent = sender.Send(msg);
+
+                Console.WriteLine("Message sent to the server : {0}", message);
+
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace ATSEmulator
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+           
+            try
+            {
+                string ip = ConfigurationManager.AppSettings["IPAddress"];
+                int portNo = Int32.Parse(ConfigurationManager.AppSettings["PortNo"]);
+                string message = Console.ReadLine();
+                Console.WriteLine(message);
+                //int port = 11000;
+
+                IPAddress ipAddress = IPAddress.Parse(ip);
+
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, portNo);
+
+                Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                sender.Connect(remoteEP);
+
+                Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(message);
+
+                int bytesSent = sender.Send(msg);
+
+                Console.WriteLine("Message sent to the server : {0}", message);
+
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
